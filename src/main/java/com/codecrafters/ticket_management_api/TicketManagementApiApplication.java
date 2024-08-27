3a05792d8b2ff47fae10d343af34f6b36214b915
Luiz Calixto <calixtoluiz0@gmail.com>
2024-08-26 23:38:45 -0300

    Ticket Main

---
+++ src/main/java/com/codecrafters/ticket_management_api/TicketManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.codecrafters.ticket_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketManagementApiApplication.class, args);
+    }
+
+}
